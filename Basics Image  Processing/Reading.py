a5c162aeeb80a4878f46e1ee9873b02478db07c4
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2022-04-07 22:44:47 +0530

    Create Reading.py
    Create Reading.py

---
+++ Basics Image  Processing/Reading.py
@@ -0,0 +1,6 @@
+## we will read the image in the first using PIL (Python Image Library)
+from PIL import Image
+image=open.Image("content/tree.png")
+image.show()  #In python
+
+image #In google Colab
